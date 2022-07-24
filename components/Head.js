8b2b05264c4b5068beda55248434e83697a91b0f
Jessica Zager <jmzager@gmail.com>
2022-07-24 12:10:27 -0500

    First changes

---
+++ components/Head.js
@@ -0,0 +1,11 @@
+export default function Head () {
+    return (
+        <head className={stlyes.head}>
+        <meta charSet="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
+            <link href="styles/styles.css" rel="stylesheet" type="text/css"></link>
+            <link rel="icon" href="images/favicon.png"/>
+            <title>I STILL LOVE STRONGBAD</title>
+        </meta>
+        </head>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,14 @@
-import Head from 'next/head'
+import Head from '@components/Head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my fan page for Stongbad! " />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing<code>pages/index.js</code>
         </p>
       </main>
 
